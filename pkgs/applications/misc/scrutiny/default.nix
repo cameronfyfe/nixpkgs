c993c4b3b1468292e576aa3d009b10ca8bfe8de2
cameronfyfe <cameron.j.fyfe@gmail.com>
2024-01-06 17:57:39 -0500

    wip

--- pkgs/applications/misc/scrutiny/default.nix
+++ pkgs/applications/misc/scrutiny/default.nix
@@ -1,21 +1,33 @@
 { lib
 , buildGoModule
 , fetchFromGitHub
+, makeWrapper
 , smartmontools
 }:
 
 buildGoModule rec {
   pname = "scrutiny";
-  version = "0.6.0";
+  version = "0.7.2";
 
   src = fetchFromGitHub {
     owner = "AnalogJ";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-zkw+41UJRXi3eQO4YEMq8iAqhFjTTMyinLGCBN4Oc0Y=";
+    sha256 = "sha256-UYKi+WTsasUaE6irzMAHr66k7wXyec8FXc8AWjEk0qs=";
   };
 
-  vendorSha256 = "sha256-eB2Zmd537psC0QU2SkWlamLC5qw0HwFo4IxWgQXfxmQ=";
+  vendorHash = "sha256-SiQw6pq0Fyy8Ia39S/Vgp9Mlfog2drtVn43g+GXiQuI=";
+
+  nativeBuildInputs = [ makeWrapper ];
+
+  buildInputs = [ smartmontools ];
+
+  postFixup = ''
+    wrapProgram $out/bin/scrutiny-collector-metrics \
+      --set PATH ${lib.makeBinPath [
+        smartmontools
+      ]}
+  '';
 
   preInstall = ''
     getMakeVar() {
@@ -28,8 +40,6 @@ buildGoModule rec {
     mv $bin/collector-metrics $bin/$COLLECTOR_BINARY_NAME
     mv $bin/scrutiny $bin/$WEB_BINARY_NAME
   '';
-
-  buildInputs = [ smartmontools ];
 
   doCheck = false;
 
